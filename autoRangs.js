3e0da1d13000ce9d9a158f2c733dbcd15f2fc862
BlackBirdBrony <piesekleszek90@interia.eu>
2016-10-14 18:50:04 +0200

    Delete autoRangs.js

--- autoRangs.js
+++
@@ -1,8 +0,0 @@
-const config = require("./config.json")
-
-module.exports = function(bot) {
-	bot.on('guildMemberAdd', (guild, member) => {
-		member.setRole(config.roleID)
-		guild.channels.find("id","218445024944193536").sendMessage("Witaj "+member+" na serverze "+guild+"!")
-	});
-}
