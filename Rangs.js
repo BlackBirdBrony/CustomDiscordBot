3c314d1adb36e94e0938e19187be9fe0d4acdaa0
BlackBirdBrony <piesekleszek90@interia.eu>
2016-10-14 18:49:40 +0200

    Add files via upload

---
+++ Rangs.js
@@ -0,0 +1,33 @@
+﻿const config = require("./config.json")
+const err = require("./error.json")
+const replyMSG = require("./messages.json")
+const commands = {
+	"NSFW": {
+		process: function(msg) {
+			msg.guild.members.find("id",msg.author.id).addRoles(config.rolesID.NSFW)
+			msg.reply(replyMSG.gotRoleNSFW)
+		}
+	}
+} 
+
+module.exports = function(bot) {
+bot.on("message", msg => {
+	if(msg.content.startsWith(config.prefix)) {
+		var command = commands[msg.content.split(" ")[0].replace(config.prefix,"")]
+		if(command) {
+			try {
+				command.process(msg)
+			} catch(a) { 
+				console.log(a)
+			}
+		}
+		else {
+			msg.channel.sendMessage(err.e1)
+		}
+	}
+})
+	bot.on('guildMemberAdd', (guild, member) => {
+		member.addRoles(config.rolesID.auto)
+		console.log("Triggered!!")
+	});
+}
--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const Discord = require("discord.js");
+﻿const Discord = require("discord.js");
 const config = require("./config.json")
 const client = new Discord.Client({autoReconnect:true})
-client.login(config.token);
+client.login(config.token).catch(console.log)
 const modulesPath = config.static.modulesPath
 
-var setRangs = require(modulesPath+"autoRangs.js")
+var setRangs = require(modulesPath+"Rangs.js")
 setRangs(client)
 
-
+var dcCount = 0
 setTimeout(function() {
 		client.user.setStatus("online",config.game)
 		console.log("Set status to online and game to"+config.game)
 	}, 3000);
-var dcCount = 0
 client.on('reconnecting', () => {
 	dcCount++
     console.log("Bot disconnected for "+dcCount+"th time, reconnecting");
